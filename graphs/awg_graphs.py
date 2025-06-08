ed614983af30a7f004456a31567acbe074e8e810
Jurre <jurre.botman@hotmail.nl>
2025-06-08 15:42:13 +0200

    metingen, graphs en matched circuit

---
+++ graphs/awg_graphs.py
@@ -0,0 +1,86 @@
+import matplotlib.pyplot as plt
+import csv
+import numpy as np
+
+offset = 72
+time_offset = 0.748e-5
+
+channel_1_x = []
+channel_1_y = []
+channel_2_x = []
+channel_2_y = []
+
+with open('./ALL0001/F0001CH1.CSV', 'r') as csvfile:
+    plots = list(csv.reader(csvfile, delimiter = ','))
+    #print(plots)
+
+    for row in plots[18+offset:]:
+        #print(row[3],row[4], row[3][:1] + row[3][2:])
+
+        channel_1_x.append(float(row[3][:1] + row[3][2:])+time_offset)
+        channel_1_y.append(float(row[4])+2.7)
+
+with open('./ALL0001/F0001CH2.CSV', 'r') as csvfile:
+    plots = list(csv.reader(csvfile, delimiter = ','))
+    #print(plots)
+
+    for row in plots[18+offset:]:
+        #print(row[3],row[4], row[3][:1] + row[3][2:])
+
+        channel_2_x.append(float(row[3][:1] + row[3][2:])+time_offset)
+        channel_2_y.append(float(row[4])-0.03)
+
+
+
+read_channel_1_x = []
+read_channel_1_y = []
+read_channel_2_x = []
+read_channel_2_y = []
+
+with open('./ALL0002/F0002CH1.CSV', 'r') as csvfile:
+    plots = list(csv.reader(csvfile, delimiter = ','))
+    #print(plots)
+
+    for row in plots[18:-offset]:
+        #print(row[3],row[4], row[3][:1] + row[3][2:])
+
+        read_channel_1_x.append(float(row[3][:1] + row[3][2:])+time_offset)
+        read_channel_1_y.append(float(row[4])+1.6)
+
+
+
+
+fig, (ax1, ax3) = plt.subplots(2,1)
+
+color = 'mediumseagreen'
+ax1.plot(channel_1_x, channel_1_y, color=color, linewidth=0.8)
+#ax1.tick_params(axis='y', labelcolor=color)
+
+ax2 = ax1.twinx()
+
+color = 'goldenrod'
+ax2.plot(channel_2_x, channel_2_y, color=color, linewidth=0.8)
+#ax2.tick_params(axis='y', labelcolor=color)
+
+color = 'steelblue'
+ax3.plot(read_channel_1_x, read_channel_1_y, color=color, linewidth=0.8)
+#ax3.tick_params(axis='y', labelcolor=color)
+
+#ax4 = ax3.twinx()
+
+#color = 'tab:blue'
+#ax4.plot(read_channel_2_x, read_channel_2_y, color=color, linewidth=0.8, alpha=1)
+#ax4.tick_params(axis='y', labelcolor=color)
+
+ax1.set_xticks(np.arange(-0.6e-5, 1.7e-5, 0.1e-5))
+ax3.set_xticks(np.arange(-0.6e-5, 1.7e-5, 0.1e-5))
+ax1.tick_params(axis='x',labelsize=9)
+ax2.tick_params(axis='x',labelsize=9)
+ax3.tick_params(axis='x',labelsize=9)
+ax1.set_xlabel('Time [s]')
+ax3.set_xlabel('Time [s]')
+ax1.set_ylabel('Channel 1 Marker [V]')
+ax2.set_ylabel('Channel 1 Out [V]')
+ax3.set_ylabel('Channel 2 Marker [V]')
+fig.tight_layout()
+plt.show()
---
+++ matching_circuit.py
@@ -0,0 +1,71 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import ScalarFormatter
+
+ZL = 0.51 - 21.83j
+
+Q = np.sqrt((50 / 0.51) - 1)
+C = 1 / (2 * np.pi * 110e6 * Q * 0.51)
+L = 50 / Q / (2 * np.pi * 110e6)
+print(Q,C,L)
+
+def calculate_impedance(f):
+
+    w = 2 * np.pi * f
+
+    L_l = 31.6e-9
+    Z_ll = 1j * w * L_l
+
+    ZL2 = ZL + Z_ll
+
+    Zc = -1j / (w * C)
+    Z1 = ZL2 + Zc
+
+    Zl = 1j * w * L
+    Zin = 1 / (1 / Z1 + 1 / Zl)
+    return Zin
+
+Z0 = 50
+Zin = calculate_impedance(110e6)
+
+Gamma = (Zin - Z0) / (Zin + Z0)
+VSWR = (1 + abs(Gamma)) / (1 - abs(Gamma))
+
+print(Zin,Gamma,VSWR)
+
+freqs = np.linspace(100e6, 120e6, 500)
+Gamma_vals = []
+Zin_vals = []
+
+for f_test in freqs:
+    Zin = calculate_impedance(f_test)
+    Gamma = (Zin - Z0) / (Zin + Z0)
+    Zin_vals.append(Zin)
+    Gamma_vals.append(abs(Gamma))
+
+plt.plot(freqs / 1e6, 20 * np.log10(Gamma_vals), color='seagreen')
+plt.xlabel("Frequency (MHz)")
+plt.ylabel("Return Loss (dB)")
+plt.grid(True)
+plt.tight_layout()
+plt.show()
+
+
+Zin_vals = np.array(Zin_vals)
+Z_mag = 20 * np.log10(np.abs(Zin_vals))
+Z_phase = np.angle(Zin_vals, deg=True)
+
+fig, ax1 = plt.subplots()
+ax2 = ax1.twinx()
+
+lns1 = ax1.semilogx(freqs, Z_mag, color='teal', label = 'Magnitude')
+ax1.set_ylabel("Magnitude (dBΩ)")
+lns2 = ax2.semilogx(freqs, Z_phase, color='firebrick', label='Phase')
+ax2.set_ylabel("Phase (degrees)")
+ax1.set_xlabel("Frequency (Hz)")
+lns = lns1+lns2
+labs = [l.get_label() for l in lns]
+ax1.legend(lns, labs, loc=0)
+plt.grid(True)
+plt.tight_layout()
+plt.show()
